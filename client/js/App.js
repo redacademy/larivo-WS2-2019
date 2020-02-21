12516b71f52321902ceca8ce42ffe0d5877ea7a7
EdinK1 <edinkaymakchi@gmail.com>
2020-02-20 22:19:04 -0800

    feat: setup folder structure

---
+++ client/js/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text, SafeAreaView} from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Larivo</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
