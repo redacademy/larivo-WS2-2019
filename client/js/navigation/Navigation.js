cfec95e0ef9294ff464e6d68601c0ed75bf30b11
EdinK1 <edinkaymakchi@gmail.com>
2020-02-22 13:59:53 -0800

    feat: add bottom navigation

---
+++ client/js/navigation/Navigation.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {
+  Home,
+  BookMark,
+  StoryForm,
+  Activity,
+  Profile,
+} from '../screens'
+
+const Tab = createBottomTabNavigator()
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Home'}}
+        />
+        <Tab.Screen
+          name="Bookmark"
+          component={BookMark}
+          options={{title: 'BookMark'}}
+        />
+        <Tab.Screen
+          name="StoryForm"
+          component={StoryForm}
+          options={{title: '+'}}
+        />
+        <Tab.Screen
+          name="Activity"
+          component={Activity}
+          options={{title: 'Activity'}}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{title: 'Profile'}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Navigation
