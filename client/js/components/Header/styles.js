33b318b864a102d6963d58696e2584800191432f
EdinK1 <edinkaymakchi@gmail.com>
2020-02-22 16:55:20 -0800

    feat: add styles and export components

---
+++ client/js/components/Header/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#03dac4',
+    paddingVertical: 20,
+  },
+})
+
+export default styles
