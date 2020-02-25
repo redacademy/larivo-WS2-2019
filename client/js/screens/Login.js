6a6625085a1c91d6bbb37b1d48f5f4ebb320ed6b
EdinK1 <edinkaymakchi@gmail.com>
2020-02-24 19:35:17 -0800

    style: remove comments

--- client/js/screens/Login.js
+++ client/js/screens/Login.js
@@ -1,54 +1,47 @@
 import React from 'react'
-import {Text, View, SafeAreaView, Image} from 'react-native'
+import {Text, View, SafeAreaView} from 'react-native'
 import Larivo_logo from '../../assets/icons/login/icon-larivo_logo.svg'
-// import { SvgUri } from 'react-native-svg';
 
 const styles = {
   container: {
-    backgroundColor: '#03DAC4', 
+    backgroundColor: '#03DAC4',
     height: '100%',
-    // flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
   },
-  // slide styles
   slidesContainer: {
     alignItems: 'center',
     marginTop: 114,
-    margin: 68
+    margin: 68,
   },
   logo: {
     shadowColor: 'rgba(0,0,0,0.25)',
-    shadowOffset: {width: 2,height: 2},
-    shadowRadiusa: 7
+    shadowOffset: {width: 2, height: 2},
+    shadowRadiusa: 7,
   },
   headline: {
     fontSize: 32,
-    marginTop: 25
+    marginTop: 25,
   },
   bodyText: {
     marginTop: 10,
-    fontSize: 16
-  }
+    fontSize: 16,
+  },
 }
 
 const Login = () => {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-
-        {/* slide */}
         <View style={styles.slidesContainer}>
           <Larivo_logo style={styles.logo} />
-          <Text style={styles.headline}>
-            Larivo
-          </Text>
+          <Text style={styles.headline}>Larivo</Text>
           <Text style={styles.bodyText}>
-          A safe platform to share curated mental health stories and find mental health resources.
+            A safe platform to share curated mental health stories and
+            find mental health resources.
           </Text>
         </View>
-
-       </View>
+      </View>
     </SafeAreaView>
   )
 }
