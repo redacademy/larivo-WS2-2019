b8b58aaa8c8c4acc58c4c382b7e785ae0909e477
jose <jmvr37@hotmail.com>
2020-02-26 19:04:30 -0800

    add the follow component to components file

--- client/js/components/StoryTitle/styles.js
+++ client/js/components/StoryTitle/styles.js
@@ -2,7 +2,7 @@ import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
   textTitle: {
-    fontSize: 25,
+    fontSize: 23,
     color: '#13443c',
     fontWeight: 'bold',
     paddingTop: 6,
