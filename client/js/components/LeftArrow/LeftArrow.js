7817498ab94c201204e8e6e260fdc945b9cf777d
Ricky Chan <ric0611@gmail.com>
2020-02-27 21:05:22 -0800

    add left arrow

---
+++ client/js/components/LeftArrow/LeftArrow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, TouchableOpacity } from 'react-native'
+import styles from './styles'
+import LeftArrowIcon from '../../../assets/icons/profile/left-arrow.svg'
+
+const LeftArrow = () => (
+  <LeftArrowIcon  width={30} height={15}/>
+)
+
+export default LeftArrow
---
+++ client/js/components/LeftArrow/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native'
+
+const styles = (props) => StyleSheet.create({
+  arrow : {
+    width: 10,
+    height: 20
+  }
+})
+
+export default styles
