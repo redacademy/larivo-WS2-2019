fc1fb021262de143c410e93b76b3d9d17500f020
EdinK1 <edinkaymakchi@gmail.com>
2020-02-27 20:23:33 -0800

    feat: create tabs for profile page

---
+++ client/js/navigation/ProfileTabs.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+import {
+  ProfileStory,
+  ProfileDraft,
+  ProfileBookMark,
+  ProfileActivity,
+} from '../screens'
+
+const Tab = createMaterialTopTabNavigator()
+
+const ProfileTabs = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      showIcon: true,
+      // showLabel: false,
+      tabStyle: {
+        height: 45,
+        borderBottomWidth: 0.3,
+        borderBottomColor: '#1E6A62',
+        borderTopWidth: 0,
+      },
+      indicatorStyle: {backgroundColor: '#1E6A62'},
+    }}
+  >
+    <Tab.Screen
+      initialRouteName="ProfileStory"
+      name="My Stories"
+      component={ProfileStory}
+    />
+    <Tab.Screen name="My Drafts" component={ProfileDraft} />
+    <Tab.Screen name="Bookmarks" component={ProfileBookMark} />
+    <Tab.Screen name="Activities" component={ProfileActivity} />
+  </Tab.Navigator>
+)
+
+export default ProfileTabs
