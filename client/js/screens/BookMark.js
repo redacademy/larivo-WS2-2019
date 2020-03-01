0bccf5817fb194c5aa740d825c746dec65fa0836
Ricky Chan <ric0611@gmail.com>
2020-02-29 16:07:41 -0800

    fixing setting title

--- client/js/screens/BookMark.js
+++ client/js/screens/BookMark.js
@@ -1,32 +1,11 @@
 import React from 'react'
-import { Text, SafeAreaView, View } from 'react-native'
-import LeftArrow from '../components/LeftArrow'
-import BellIcon from '../../assets/icons/profile/icon-profile-bell.svg'
-import UserIcon from '../../assets/icons/profile/icon-profile-user.svg'
-import DeleteIcon from '../../assets/icons/profile/icon-profile-delete.svg'
-import SettingItem from '../components/SettingItem'
-
+import Settings from './Settings'
 import styles from './styles'
 
 // to be changed to settings component
 const BookMark = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.settings_container}>
-        <View style={styles.settings_leftArrow}>
-          <LeftArrow />
-        </View>
-        <View style={styles.settings_contentContainer}>
-          <Text style={styles.settings_title}>
-            Setting
-          </Text>
-          <SettingItem icon={BellIcon} text='Notifications' />
-          <SettingItem icon={UserIcon} text='Edit Profile' />
-          <SettingItem icon={DeleteIcon} text='Delete Account' />
-          <View style={styles.settings_itemsConstainer} />
-        </View>
-      </View>
-    </SafeAreaView>
+    <Settings />
   )
 }
 
