c69a653d94bf0c4481519181d041b56ccd96ca80
EdinK1 <edinkaymakchi@gmail.com>
2020-03-01 21:21:41 -0800

    feat: change button styles

--- client/js/components/Button/styles.js
+++ client/js/components/Button/styles.js
@@ -1,25 +1,26 @@
 import {StyleSheet} from 'react-native'
 
-const styles = (props) => StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 44,
-    width: 214,
-    borderRadius: 20.75,
-    backgroundColor: props.backgroundColor,
-    shadowOpacity: 0.75,
-    shadowRadius: 5,
-    shadowColor: 'rgba(0,0,0,0.25)',
-    shadowOffset: { height: 2, width: 2 },
-    marginTop: 25
-  },
-  text: {
-    fontSize: 16,
-    color: props.color,
-    fontWeight: '300',
-    fontFamily: 'SFProText-Light',
-  }
-})
+const styles = props =>
+  StyleSheet.create({
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingHorizontal: 20,
+      paddingVertical: 12,
+      borderRadius: 20.75,
+      backgroundColor: props.backgroundColor,
+      shadowOpacity: 0.75,
+      shadowRadius: 5,
+      shadowColor: 'rgba(0,0,0,0.25)',
+      shadowOffset: {height: 2, width: 2},
+      marginTop: 25,
+    },
+    text: {
+      fontSize: 16,
+      color: props.color,
+      fontWeight: '300',
+      fontFamily: 'SFProText-Light',
+    },
+  })
 
 export default styles
