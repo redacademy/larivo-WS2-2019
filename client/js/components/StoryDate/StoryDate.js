10527e0b0d6a5a63bebfc1c56b7dc86784c92305
jose <jmvr37@hotmail.com>
2020-03-10 18:14:17 -0700

    create new folder story date in components folder

---
+++ client/js/components/StoryDate/StoryDate.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+import styles from './styles'
+
+const StoryDate = () => (
+  <View>
+    <Text>im the story date</Text>
+  </View>
+)
+
+export default StoryDate
