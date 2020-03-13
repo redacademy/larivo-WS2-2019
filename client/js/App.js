8f39b6aab2db0f28b2026c74486add22f3eba347
EdinK1 <edinkaymakchi@gmail.com>
2020-03-12 18:26:00 -0700

    feat: wrap app with apollo provider

--- client/js/App.js
+++ client/js/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import {ApolloProvider} from '@apollo/react-hooks'
 import {Navigation} from './navigation'
+import {client, AuthContextProvider} from './context'
 
 const App = () => {
   return (
-    <>
-      <Navigation />
-    </>
+    <ApolloProvider client={client}>
+      <AuthContextProvider>
+        <Navigation />
+      </AuthContextProvider>
+    </ApolloProvider>
   )
 }
 
