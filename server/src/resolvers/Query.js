5f4c8f093ae6e4477162af1d950955fac1e44dc1
EdinK1 <edinkaymakchi@gmail.com>
2020-03-14 03:21:34 -0700

    feat: update resolvers

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,14 +1,9 @@
 const { getUserId } = require('../utils')
 
 const Query = {
-  feed(parent, args, context) {
-    const id = getUserId(context)
-    // const orderBy = {
+  guestFeed(parent, args, context) {
     const where = {
-      published: true,
-      author: {
-        id
-      }
+      published: true
     }
     return context.prisma.stories({
       where,
@@ -16,6 +11,21 @@ const Query = {
       first: 25
     })
   },
+
+  async userFeed(parent, args, context) {
+    const id = getUserId(context)
+    const stories = await context.prisma.user({ id }).stories()
+    const where = {
+      published: true,
+      id_not_in: stories.map(story => story.id)
+    }
+    return context.prisma.stories({
+      where,
+      orderBy: 'createdAt_DESC',
+      first: 25
+    })
+  },
+
   drafts(parent, args, context) {
     const id = getUserId(context)
     const where = {
@@ -26,16 +36,20 @@ const Query = {
     }
     return context.prisma.stories({ where })
   },
+
   users(parent, args, context) {
     return context.prisma.users()
   },
+
   story(parent, { id }, context) {
     return context.prisma.story({ id })
   },
+
   me(parent, args, context) {
     const id = getUserId(context)
     return context.prisma.user({ id })
   },
+
   async filteredStories(parent, args, context) {
     const id = getUserId(context)
     const hashtags = await context.prisma.user({ id }).hashtags()
