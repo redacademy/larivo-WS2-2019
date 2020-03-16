c89a95ff747e4cbabd345f91c213106f43229eb2
EdinK1 <edinkaymakchi@gmail.com>
2020-03-16 15:11:25 -0700

    feat: update profileDraft and profileStory screens

--- client/js/screens/Profile/ProfileDraft.js
+++ client/js/screens/Profile/ProfileDraft.js
@@ -1,8 +1,39 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {View, Text, FlatList, TouchableOpacity} from 'react-native'
+import {USER_DRAFTS} from '../../context/apollo'
+import {useQuery} from '@apollo/react-hooks'
+import {Spinner} from '../../components/Spinner'
+import {NetWorkError} from '../../components/FourOhFour'
 
-const ProfileDraft = () => {
-  return <Text>ProfileDraft</Text>
+const ProfileDraft = ({navigation}) => {
+  const {loading, error, data} = useQuery(USER_DRAFTS)
+
+  if (loading) return <Spinner />
+  if (error) return <NetWorkError />
+  const {drafts} = data.me
+
+  return (
+    <FlatList
+      data={drafts}
+      renderItem={({item: {id, title, createdAt, content}}) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate('HomeStory', {id})}
+        >
+          <View
+            style={{
+              borderBottomColor: '#000',
+              borderBottomWidth: 1,
+            }}
+          >
+            <Text>{title}</Text>
+            <Text>{createdAt}</Text>
+            <Text>{content}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+      keyExtractor={item => item.id}
+    />
+  )
 }
 
 export default ProfileDraft
--- client/js/screens/Profile/ProfileStory.js
+++ client/js/screens/Profile/ProfileStory.js
@@ -1,8 +1,39 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {View, Text, FlatList, TouchableOpacity} from 'react-native'
+import {USER_STORIES} from '../../context/apollo'
+import {useQuery} from '@apollo/react-hooks'
+import {Spinner} from '../../components/Spinner'
+import {NetWorkError} from '../../components/FourOhFour'
 
-const ProfileStory = () => {
-  return <Text>ProfileStory</Text>
+const ProfileStory = ({navigation}) => {
+  const {loading, error, data} = useQuery(USER_STORIES)
+
+  if (loading) return <Spinner />
+  if (error) return <NetWorkError />
+  const {stories} = data.me
+
+  return (
+    <FlatList
+      data={stories}
+      renderItem={({item: {id, title, createdAt, content}}) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate('HomeStory', {id})}
+        >
+          <View
+            style={{
+              borderBottomColor: '#000',
+              borderBottomWidth: 1,
+            }}
+          >
+            <Text>{title}</Text>
+            <Text>{createdAt}</Text>
+            <Text>{content}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+      keyExtractor={item => item.id}
+    />
+  )
 }
 
 export default ProfileStory
