0fba69db7b55f0ae72bac9d5884199d9d06d6e96
EdinK1 <edinkaymakchi@gmail.com>
2020-03-16 20:08:56 -0700

    feat: add publish mutation

--- server/src/resolvers/Mutation/story.js
+++ server/src/resolvers/Mutation/story.js
@@ -71,21 +71,21 @@ const story = {
     })
   },
 
-  // async publish(parent, { id }, context) {
-  //   const userId = getUserId(context)
-  //   const storyExists = await context.prisma.$exists.story({
-  //     id,
-  //     author: { id: userId }
-  //   })
-  //   if (!storyExists) {
-  //     throw new Error(`Post not found or you're not the author`)
-  //   }
+  async publish(parent, { id }, context) {
+    const userId = getUserId(context)
+    const storyExists = await context.prisma.$exists.story({
+      id,
+      author: { id: userId }
+    })
+    if (!storyExists) {
+      throw new Error(`Post not found or you're not the author`)
+    }
 
-  //   return context.prisma.updateStory({
-  //     where: { id },
-  //     data: { published: true }
-  //   })
-  // },
+    return context.prisma.updateStory({
+      where: { id },
+      data: { published: true }
+    })
+  },
 
   async like(parent, { id }, context) {
     const userId = getUserId(context)
