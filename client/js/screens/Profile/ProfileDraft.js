d644579c6c5c2f155eef42681b3efd2990e89c46
EdinK1 <edinkaymakchi@gmail.com>
2020-03-16 20:10:56 -0700

    feat: update profile draft

--- client/js/screens/Profile/ProfileDraft.js
+++ client/js/screens/Profile/ProfileDraft.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import readingTime from 'reading-time'
 import {View, Text, FlatList, TouchableOpacity} from 'react-native'
 import {USER_DRAFTS} from '../../context/apollo'
 import {useQuery} from '@apollo/react-hooks'
@@ -15,22 +16,26 @@ const ProfileDraft = ({navigation}) => {
   return (
     <FlatList
       data={drafts}
-      renderItem={({item: {id, title, createdAt, content}}) => (
-        <TouchableOpacity
-          onPress={() => navigation.navigate('HomeStory', {id})}
-        >
-          <View
-            style={{
-              borderBottomColor: '#000',
-              borderBottomWidth: 1,
-            }}
+      renderItem={({item: {id, title, createdAt, content}}) => {
+        const {text: readTime} = readingTime(content)
+        return (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SingleDraft', {id})}
           >
-            <Text>{title}</Text>
-            <Text>{createdAt}</Text>
-            <Text>{content}</Text>
-          </View>
-        </TouchableOpacity>
-      )}
+            <View
+              style={{
+                borderBottomColor: '#000',
+                borderBottomWidth: 1,
+              }}
+            >
+              <Text>{title}</Text>
+              <Text>{createdAt}</Text>
+              <Text>{readTime}</Text>
+              <Text>{content}</Text>
+            </View>
+          </TouchableOpacity>
+        )
+      }}
       keyExtractor={item => item.id}
     />
   )
