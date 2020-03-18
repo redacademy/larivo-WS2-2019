a0cd31564a5969ad07d4b788641e165190d28466
EdinK1 <edinkaymakchi@gmail.com>
2020-03-18 01:15:46 -0700

    feat: update header profile component

--- client/js/components/HeaderProfile/HeaderProfile.js
+++ client/js/components/HeaderProfile/HeaderProfile.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import {Text, View, TouchableOpacity, ScrollView} from 'react-native'
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native'
 import NameInitials from '../NameInitials/NameInitials'
 import styles from './styles'
 import Paragraph from '../../components/Paragraph/Paragraph'
@@ -15,40 +21,39 @@ const HeaderProfile = ({navigation}) => {
 
   if (typeof user === 'undefined') return <Spinner />
   return (
-    <View style={styles.headerContainer}>
-      <View style={styles.titleContainer}>
-        <NameInitials>RV</NameInitials>
-        <View style={styles.followsContainer}>
-          <Text style={styles.numbers}>53</Text>
-          <Text style={styles.textFollow}>Stories</Text>
+    <SafeAreaView style={styles.header}>
+      <View style={styles.headerContainer}>
+        <View style={styles.titleContainer}>
+          <NameInitials>{user.user.userName}</NameInitials>
+          <View style={styles.followsContainer}>
+            <Text style={styles.numbers}>53</Text>
+            <Text style={styles.textFollow}>Stories</Text>
+          </View>
+          <View style={styles.followsContainer}>
+            <Text style={styles.numbers}>64.5K</Text>
+            <Text style={styles.textFollow}>Followers</Text>
+          </View>
+          <View style={styles.followsContainer}>
+            <Text style={styles.numbers}>1247</Text>
+            <Text style={styles.textFollow}>Following</Text>
+          </View>
+          <View>
+            <SettingsIcon
+              onPress={() => navigation.navigate('Settings')}
+            />
+          </View>
         </View>
-        <View style={styles.followsContainer}>
-          <Text style={styles.numbers}>64.5K</Text>
-          <Text style={styles.textFollow}>Followers</Text>
-        </View>
-        <View style={styles.followsContainer}>
-          <Text style={styles.numbers}>1247</Text>
-          <Text style={styles.textFollow}>Following</Text>
-        </View>
-        <View>
-          <SettingsIcon
-            onPress={() => navigation.navigate('Settings')}
+
+        <View style={styles.titleContainer}>
+          <StoryTitle>{user.user.userName}</StoryTitle>
+
+          <EditIcon
+            onPress={() => navigation.navigate('Bio', {user})}
           />
         </View>
+        <Paragraph>{user.user.bio || 'lorem'}</Paragraph>
       </View>
-
-      <View style={styles.titleContainer}>
-        <StoryTitle>Roaring Velvet</StoryTitle>
-
-        <EditIcon
-          onPress={() => navigation.navigate('Bio', {user})}
-        />
-      </View>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </Paragraph>
-    </View>
+    </SafeAreaView>
   )
 }
 
