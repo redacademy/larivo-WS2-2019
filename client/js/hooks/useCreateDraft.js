75c9140c297dcbebcb53ae789a9a0bfd1acdb9bd
EdinK1 <edinkaymakchi@gmail.com>
2020-03-18 01:15:59 -0700

    feat: update hooks

--- client/js/hooks/useCreateDraft.js
+++ client/js/hooks/useCreateDraft.js
@@ -2,7 +2,9 @@ import {useMutation} from '@apollo/react-hooks'
 import {CREATE_DRAFT} from '../context'
 
 const useCreateDraft = () => {
-  const [createDraft, {data, error}] = useMutation(CREATE_DRAFT)
+  const [createDraft, {data, error}] = useMutation(CREATE_DRAFT, {
+    refetchQueries: ['me'],
+  })
   const handleCreateDraft = args => createDraft(args)
   return [handleCreateDraft, data, error]
 }
--- client/js/hooks/useCreateStory.js
+++ client/js/hooks/useCreateStory.js
@@ -3,7 +3,7 @@ import {CREATE_STORY} from '../context'
 
 const useCreateStory = () => {
   const [createStory, {data, error}] = useMutation(CREATE_STORY, {
-    refetchQueries: ['userFeed, guestFeed'],
+    refetchQueries: ['userFeed, guestFeed', 'me'],
   })
   const handleCreateStory = args => createStory(args)
   return [handleCreateStory, data, error]
