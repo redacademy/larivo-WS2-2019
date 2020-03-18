66b6e27c70b13a8410eb0e83cb917d5d47660c74
jose <jmvr37@hotmail.com>
2020-03-17 18:05:11 -0700

    create a folder for profile header

---
+++ client/js/components/HeaderProfile/HeaderProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text, View, TouchableOpacity} from 'react-native'
+import NameInitials from '../NameInitials/NameInitials'
+
+
+const HeaderProfile = () =>(
+
+
+)
+
+export default HeaderProfile
