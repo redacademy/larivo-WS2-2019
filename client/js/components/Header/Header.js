9c139f264290bf76324f4a0526cf43a1b315b4a9
EdinK1 <edinkaymakchi@gmail.com>
2020-03-19 15:54:03 -0700

    feat: update header

--- client/js/components/Header/Header.js
+++ client/js/components/Header/Header.js
@@ -1,17 +1,36 @@
-import React from 'react'
-import {View} from 'react-native'
+import React, {useState} from 'react'
+import {View, TextInput} from 'react-native'
 import styles from './styles'
 import NameInitials from '../NameInitials/NameInitials'
-import InputSearchField from '../InputField/InputSearchField'
+import {SearchTabs} from '../../navigation'
 
-const Header = ({userName, navigation}) => {
+const Header = ({userName, navigation, search, setSearch}) => {
   return (
-    <View style={styles.header}>
-      <View style={styles.searchBox}>
-        <NameInitials onPress={() => navigation.navigate('Profile')}>
-          {userName}
-        </NameInitials>
-        <InputSearchField placeholder="Search for topics of interest" />
+    <View>
+      <View style={styles.header}>
+        <View style={styles.searchBox}>
+          <NameInitials
+            onPress={() => navigation.navigate('Profile')}
+          >
+            {userName}
+          </NameInitials>
+          <TextInput
+            value={search}
+            onChangeText={text => setSearch(text)}
+            style={{
+              backgroundColor: '#fff',
+              borderRadius: 20.5,
+              fontSize: 14,
+              marginLeft: 15,
+              paddingVertical: 10,
+              minWidth: 255,
+              width: 275,
+              paddingLeft: 15,
+            }}
+            placeholder="Search for topics of interest"
+            inline
+          />
+        </View>
       </View>
     </View>
   )
