d2e33818f43f4b2fad566b073091a733a2e1ee12
EdinK1 <edinkaymakchi@gmail.com>
2020-03-22 16:44:58 -0700

    feat: style register screens

--- client/js/screens/Register/Username.js
+++ client/js/screens/Register/Username.js
@@ -1,37 +1,73 @@
 import React from 'react'
-import {Text, View, SafeAreaView} from 'react-native'
-import InputFieldEmail from '../../components/InputField/InputFieldEmail'
+import {TextInput, Text, View, SafeAreaView} from 'react-native'
 import Button from '../../components/Button'
+import {Formik} from 'formik'
 import styles from './styles'
-import {UserNameForm} from '../../components/UserNameForm'
+import * as yup from 'yup'
 
 const Username = ({route, navigation}) => {
+  const {email, password} = route.params
+
+  const UserNameSchema = yup.object().shape({
+    userName: yup.string().required('Username is required'),
+  })
+
+  const handleUserName = values => {
+    navigation.navigate('HashtagScreen', {
+      email,
+      password,
+      userName: values.userName,
+    })
+  }
+
   return (
     <SafeAreaView>
-      {/* <View style={styles.registerContainer}>
-        <Text style={styles.textStyleUsernameScreen}>
-          generate your username
-        </Text>
-        <View style={styles.childContainer}>
-          <View style={{paddingVertical: 40}}>
-            <InputFieldEmail placeholder={'Roaring Velvet'} />
+      <Formik
+        onSubmit={handleUserName}
+        initialValues={{userName: ''}}
+        validationSchema={UserNameSchema}
+      >
+        {({
+          handleChange,
+          values,
+          handleSubmit,
+          errors,
+          touched,
+          setFieldTouched,
+        }) => (
+          <View style={styles.registerContainer}>
+            <Text style={styles.title}>enter your username</Text>
+            <View style={styles.childContainer}>
+              <View style={styles.innerChild}>
+                <Text style={styles.textStyle}>
+                  Enter Your Username
+                </Text>
+                <View style={styles.inputBox}>
+                  <TextInput
+                    style={styles.input}
+                    onChangeText={handleChange('userName')}
+                    onBlur={() => setFieldTouched('userName')}
+                    value={values.userName}
+                    autoCapitalize="none"
+                    placeholder="Roaring Velvet"
+                  />
+                  {touched.userName && errors.userName && (
+                    <Text style={styles.errorText}>
+                      {errors.userName}
+                    </Text>
+                  )}
+                </View>
+              </View>
+              <View style={styles.btnPaddingRegister}>
+                <Button onPress={handleSubmit} theme="light">
+                  Next
+                </Button>
+              </View>
+            </View>
           </View>
-          <View style={styles.btnPaddingUsername}>
-            <Button theme="dark">Generate Another Username</Button>
-          </View>
-          <View style={styles.btnPaddingUsername}>
-            <Button
-              onPress={() => navigation.navigate('HashtagScreen')}
-              theme="light"
-            >
-              Next
-            </Button>
-          </View>
-        </View>
-      </View> */}
-      <UserNameForm route={route} navigation={navigation} />
+        )}
+      </Formik>
     </SafeAreaView>
   )
 }
-
 export default Username
