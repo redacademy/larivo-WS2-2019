a2c555b3eb9322e081ce7318d4fdcc80f2f2db7b
EdinK1 <edinkaymakchi@gmail.com>
2020-03-24 02:14:09 -0700

    feat: update headerProfile comp

--- client/js/components/HeaderProfile/HeaderProfile.js
+++ client/js/components/HeaderProfile/HeaderProfile.js
@@ -1,39 +1,40 @@
 import React from 'react'
-import {
-  Text,
-  View,
-  TouchableOpacity,
-  ScrollView,
-  SafeAreaView,
-} from 'react-native'
+import {Text, View} from 'react-native'
 import NameInitials from '../NameInitials/NameInitials'
 import styles from './styles'
 import Paragraph from '../../components/Paragraph/Paragraph'
 import SettingsIcon from '../../components/SettingsIcon/SettingsIcon'
 import StoryTitle from '../../components/StoryTitle'
 import EditIcon from '../../components/EditIcon/EditIcon'
-import {useAuth} from '../../hooks'
+import {USER_HEADER_INFO} from '../../context/apollo'
+import {useQuery} from '@apollo/react-hooks'
 import {Spinner} from '../../components/Spinner'
+import {NetWorkError} from '../FourOhFour'
 
 const HeaderProfile = ({navigation}) => {
-  const {user} = useAuth()
-  console.log('profile user', user)
+  const {loading, error, data} = useQuery(USER_HEADER_INFO)
 
-  if (typeof user === 'undefined') return <Spinner />
+  if (loading) return null
+  if (error) return <NetWorkError />
+
   return (
     <View style={styles.header}>
       <View style={styles.titleContainer}>
-        <NameInitials>{user.user.userName}</NameInitials>
+        <NameInitials>{data.me.userName}</NameInitials>
         <View style={styles.followsContainer}>
-          <Text style={styles.numbers}>53</Text>
+          <Text style={styles.numbers}>{data.me.stories.length}</Text>
           <Text style={styles.textFollow}>Stories</Text>
         </View>
         <View style={styles.followsContainer}>
-          <Text style={styles.numbers}>64.5K</Text>
+          <Text style={styles.numbers}>
+            {data.me.followers.length}
+          </Text>
           <Text style={styles.textFollow}>Followers</Text>
         </View>
         <View style={styles.followsContainer}>
-          <Text style={styles.numbers}>1247</Text>
+          <Text style={styles.numbers}>
+            {data.me.following.length}
+          </Text>
           <Text style={styles.textFollow}>Following</Text>
         </View>
         <View>
@@ -44,13 +45,11 @@ const HeaderProfile = ({navigation}) => {
       </View>
 
       <View style={styles.titleContainer}>
-        <StoryTitle>{user.user.userName}</StoryTitle>
+        <StoryTitle>{data.me.userName}</StoryTitle>
 
-        <EditIcon
-          onPress={() => navigation.navigate('Bio', {user})}
-        />
+        <EditIcon onPress={() => navigation.navigate('Bio')} />
       </View>
-      <Paragraph>{user.user.bio || 'lorem'}</Paragraph>
+      <Paragraph>{data.me.bio || 'lorem'}</Paragraph>
     </View>
   )
 }
