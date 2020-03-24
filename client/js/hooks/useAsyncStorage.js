eb56a76de7ffd1ca0987c0d3d22a0cd26c3d467e
jose <jmvr37@hotmail.com>
2020-03-24 16:38:23 -0700

    remove the bio from the activity screen

--- client/js/hooks/useAsyncStorage.js
+++ client/js/hooks/useAsyncStorage.js
@@ -1,34 +1,34 @@
-import {useState, useEffect} from 'react'
-import AsyncStorage from '@react-native-community/async-storage'
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
 
 const useAsyncStorage = key => {
-  const [value, setValue] = useState()
+  const [value, setValue] = useState();
 
   useEffect(() => {
     async function getLocalVal() {
       try {
-        const returnedVal = await AsyncStorage.getItem(key)
-        setValue(JSON.parse(returnedVal))
+        const returnedVal = await AsyncStorage.getItem(key);
+        setValue(JSON.parse(returnedVal));
       } catch (e) {
-        console.error(e)
+        console.error(e);
       }
     }
-    getLocalVal()
-  }, [setValue, key])
+    getLocalVal();
+  }, [setValue, key]);
 
   const set = async newVal => {
     try {
       const returnedVal = await AsyncStorage.setItem(
         key,
-        JSON.stringify(newVal),
-      )
-      setValue(returnedVal)
-      return returnedVal
+        JSON.stringify(newVal)
+      );
+      setValue(returnedVal);
+      return returnedVal;
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }
-  return [value, set]
-}
+  };
+  return [value, set];
+};
 
-export default useAsyncStorage
+export default useAsyncStorage;
--- client/js/screens/Activity/Activity.js
+++ client/js/screens/Activity/Activity.js
@@ -1,16 +1,94 @@
-import React from 'react'
-import Bio from '../Profile/Bio'
-import {View, Text, ScrollView} from 'react-native'
-import StoryDate from '../../components/StoryDate'
-import {Register, Username, HashtagScreen} from '../Register'
+import React, { useState, useEffect } from "react";
+import styles from "./styles";
+import readingTime from "reading-time";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+import { StoryCard } from "../../components/StoryCard";
+import { Header } from "../../components/Header";
+import Hashtag from "../../components/Hashtag";
+import { useAuth } from "../../hooks";
+import { FILTERED_STORIES } from "../../context/apollo";
+import { useQuery } from "@apollo/react-hooks";
+import { Spinner } from "../../components/Spinner";
+import { NetWorkError } from "../../components/FourOhFour";
+import { SearchTabs } from "../../navigation";
 
-const Activity = () => {
+const Activity = ({ navigation, route }) => {
+  const [search, setSearch] = useState("");
+  const { user } = useAuth();
+  const { loading, error, data, refetch, fetchMore, networkStatus } = useQuery(
+    FILTERED_STORIES
+  );
+
+  if (loading || typeof user === "undefined") return <Spinner />;
+  if (error) return <NetWorkError />;
+
+  const { userName } = user.user;
+
   return (
-    <ScrollView>
-      <Bio />
-      <StoryDate />
-    </ScrollView>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <Header
+        navigation={navigation}
+        userName={userName}
+        search={search}
+        setSearch={setSearch}
+      />
+      {search.length ? (
+        <SearchTabs route={route} navigation={navigation} search={search} />
+      ) : (
+        <View style={styles.listView}>
+          <FlatList
+            // ListHeaderComponent={() => (
+            //   <Text style={styles.title}>Featured</Text>
+            // )}
+            // refreshing={networkStatus === 4}
+            // onRefresh={() => refetch()}
+            // onEndReached={() =>
+            //   fetchMore({
+            //     updateQuery: (prev, {fetchMoreResult}) => {
+            //       if (!fetchMoreResult) return prev
+            //       return Object.assign({}, prev, {
+            //         userFeed: [
+            //           ...prev.userFeed,
+            //           ...fetchMoreResult.userFeed,
+            //         ],
+            //       })
+            //     },
+            //   })
+            // }
+            data={data.filteredStories}
+            renderItem={({
+              item: { id, author, title, createdAt, content, hashtags }
+            }) => {
+              const { text: readTime } = readingTime(content);
+              return (
+                <TouchableOpacity
+                  style={styles.cardContainer}
+                  onPress={() => navigation.navigate("HomeStory", { id })}
+                >
+                  <StoryCard
+                    userName={author.userName}
+                    createdAt={createdAt}
+                    readTime={readTime}
+                    title={title}
+                    content={content}
+                    hashtags={hashtags}
+                    bookmarked={null}
+                  />
+                </TouchableOpacity>
+              );
+            }}
+            keyExtractor={item => item.id}
+          />
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
 
-export default Activity
+export default Activity;
