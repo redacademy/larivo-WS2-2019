ada1d2afab8b60ae84bc275f29e9ed187435921b
Ricky Chan <ric0611@gmail.com>
2020-03-23 23:52:27 -0700

    fix story card

--- client/js/components/StoryCard/StoryCard.js
+++ client/js/components/StoryCard/StoryCard.js
@@ -19,24 +19,6 @@ import {
 
 let theme = {}
 
-const formatDate = (date) => {
-  let newDate = new Date (date)
-  const month = newDate.toLocaleString('default', { month: 'short' });
-  const day = toOrdinalSuffix(newDate.getDay())
-  return (month + '. ' + day + ' ' + newDate.getFullYear()).toUpperCase()
-}
-
-const toOrdinalSuffix = num => {
-  const int = parseInt(num),
-    digits = [int % 10, int % 100],
-    ordinals = ['st', 'nd', 'rd', 'th'],
-    oPattern = [1, 2, 3, 4],
-    tPattern = [11, 12, 13, 14, 15, 16, 17, 18, 19];
-  return oPattern.includes(digits[0]) && !tPattern.includes(digits[1])
-    ? int + ordinals[digits[0] - 1]
-    : int + ordinals[3];
-}
-
 const StoryCard = ({ 
     userName, 
     title, 
